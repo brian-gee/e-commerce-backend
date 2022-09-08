2527cf320970ec28f5c515b72aeab1a09dbb20a2
Brian Gee <briangeertsma@gmail.com>
2022-09-08 17:47:18 -0400

    Updated tag and product relation

--- models/index.js
+++ models/index.js
@@ -13,15 +13,15 @@ Product.belongsTo(Category, {
 Category.hasMany(Product, {});
 
 // Products belongToMany Tags (through ProductTag)
-Tag.belongsToMany(Product, {
+Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'tag_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
-Product.belongsToMany(Tag, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'product_id'
 });
 
-module.exports = { Product, Category, Tag, ProductTag };
+module.exports = { Category, Product, Tag, ProductTag };

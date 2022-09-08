9bdc1b72e53ff651419fd1499650ffc3b8e19a3b
Brian Gee <briangeertsma@gmail.com>
2022-09-08 15:41:15 -0400

    updated tag and product foreign keys

--- models/index.js
+++ models/index.js
@@ -6,23 +6,22 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'id'
-});
-
-// Categories have many Products
-Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
 
+// Categories have many Products
+Category.hasMany(Product, {});
+
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag});
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+module.exports = { Product, Category, Tag, ProductTag };
